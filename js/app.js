a912f69523f876eb6b1ae64527d7251b76090e43
Tiago Gago <138620862+tiagogago@users.noreply.github.com>
2024-11-01 16:39:10 +0100

    commit6

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-// const menuLinks = document.querySelectorAll(' .menu a[href^="#"]');
+const menuLinks = document.querySelectorAll(' .menu a[href^="#"]');
 
-// function getDistanceFromTheTop(element) {
-//   const id = element.getAttribte("href");
-//   return document.querySelector(id).offsetTop;
-// }
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribte("href");
+  return document.querySelector(id).offsetTop;
+}
 
-// function nativeScroll(distanceFromTheTop) {
-//   window.scrollTo({ top: distanceFromTheTop, behavior: "smooth" });
-// }
+function nativeScroll(distanceFromTheTop) {
+  window.scrollTo({ top: distanceFromTheTop, behavior: "smooth" });
+}
 
-// function scrollToSection(event) {
-//   event.preventDefault();
-//   const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-//   nativeScroll(distanceFromTheTop);
-// }
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+  nativeScroll(distanceFromTheTop);
+}
 
-// menuLinks.forEach((link) => {
-//   link.addEventListener("click", scrollToSection);
-// });
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
